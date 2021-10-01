5449f96102669239eb87299853a64e3beb9bcdc2
Bhautik <bhautik@sharepointempower.com>
2021-10-01 15:27:07 +0530

    first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1>Hello Bhautik</h1>
+// }
+
+
+//function components
+const Greet = () => <h1>Hello Bhautik</h1>
+
+export default Greet
