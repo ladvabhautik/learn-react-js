2ef17474dedf35bd4e1d760ecd8afcf03e3f97a8
Bhautik <bhautik@sharepointempower.com>
2021-10-19 11:21:03 +0530

    PPP-01-ReactSetState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Counter from './components/Counter';
 import Greet from './components/Greet';
 import Hello from './components/Hello';
 import Message from './components/Message';
@@ -7,21 +8,22 @@ import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
+      <Counter />
+      <Message />
       {/* <Greet name="Bruce" heroName="Batman" >
         <p>This is Paragraph</p>
       </Greet>
       <Greet name="Clark" heroName="Superman" >
         <button>button</button>
-      </Greet>
-      <Greet name="Diana" heroName="Wonder Woman" ></Greet>
-      <Welcome name="Bruce" heroName="Batman">
+      </Greet> */}
+      {/* <Greet name="Diana" heroName="Wonder Woman" ></Greet> */}
+      {/* <Welcome name="Bruce" heroName="Batman">
         <p>This is Paragraph</p>
       </Welcome>
       <Welcome name="Clark" heroName="Superman"></Welcome>
       <Welcome name="Diana" heroName="Wonder Woman" ></Welcome> */}
       {/* <Hello /> */}
 
-      <Message/>
     </div >
   );
 }
