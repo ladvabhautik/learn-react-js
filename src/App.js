ba371151818b0cb05804bc4e2bfabe3ef95e6a8a
Bhautik <bhautik@sharepointempower.com>
2021-10-20 11:31:37 +0530

    ReactJS Tutorial - 13 - Event Handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import ClassClick from './components/ClassClick';
 import Counter from './components/Counter';
+import FucntionClick from './components/FucntionClick';
 import Greet from './components/Greet';
 import Hello from './components/Hello';
 import Message from './components/Message';
@@ -8,19 +10,21 @@ import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <Message />
+      <FucntionClick />
+      <ClassClick />
+      {/* <Counter /> */}
+      {/* <Message /> */}
       {/* <Greet name="Bruce" heroName="Batman" >
         <p>This is Paragraph</p>
       </Greet>
       <Greet name="Clark" heroName="Superman" >
         <button>button</button>
       </Greet> */}
-      <Greet name="Diana" heroName="Wonder Woman" ></Greet>
+      {/* <Greet name="Diana" heroName="Wonder Woman" ></Greet> */}
       {/* <Welcome name="Bruce" heroName="Batman">
         <p>This is Paragraph</p>
       </Welcome> */}
-      <Welcome name="Clark" heroName="Superman"></Welcome>
+      {/* <Welcome name="Clark" heroName="Superman"></Welcome> */}
       {/* <Welcome name="Diana" heroName="Wonder Woman" ></Welcome> */}
       {/* <Hello /> */}
 
